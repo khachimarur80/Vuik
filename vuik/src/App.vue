f7127f449520721516d6167b0ed96bf2e13f3d78
keirh80 <khachimarur80@gmail.com>
2024-02-17 19:53:44 +0100

    Project Initialization and vk-navbar-1 component

---
+++ vuik/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <vk-navbar-1/>
+</template>
+
+<script>
+import vkNavbar1 from './components/navbar/vk-navbar-1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    vkNavbar1
+  },
+  data: () => ({
+    theme: {
+      primary: '#2196F3',
+      secondary: '#1976D2',
+      accent: '#FFC107',
+      error: '#f44336',
+      warning: '#FF9800',
+      info: '#03A9F4',
+      success: '#4CAF50',
+      text: '#333',
+      text_secondary: '#666',
+      text_light: '#999',
+      text_dark: '#111',
+      background: '#f9f9f9',
+      background_dark: '#e0e0e0',
+      background_light: '#ffffff',
+      text_font: 'HelveticaNeue-Thin',
+      title_font: 'HelveticaNeue',
+    }
+  }),
+  mounted() {
+    for (let key in this.theme) {
+      document.documentElement.style.setProperty('--vk-'+key.replace('_', '-'), this.theme[key])
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+html, body {
+  margin: 0px;
+  padding: 0px;
+}
+</style>
---
+++ vuik/src/components/navbar/vk-navbar-1.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-header">
+      <div class="navbar-logo">
+        <img :alt="logo.alt" :src="logo.src" height="100%"/>
+      </div>
+      <div class="navbar-title">
+        Aurie
+      </div>
+    </div>
+    <div class="navbar-items">
+      <a class="navbar-item" v-for="(item, i) in items" :href="item.href" :key="i">
+        {{ item.name }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: () => ({
+    logo: {
+      src: require('@/assets/logo.png'),
+      lazySrc: require('@/assets/logo.png'),
+      alt: '',
+    },
+    items: [
+      { name: 'Item 1', href: '/#'},
+      { name: 'Item 2', href: '/#'},
+      { name: 'Item 3', href: '/#'},
+      { name: 'Item 4', href: '/#'},
+      { name: 'Item 5', href: '/#'},
+    ]
+  })
+}
+</script>
+
+<style scoped>
+  .navbar {
+    height: 100px;
+    background: var(--vk-primary);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    overflow: hidden;
+  }
+  .navbar-header {
+    width: fit-content;
+    height: 100% ;
+    box-sizing: border-box;
+    padding: 10px;
+    display: flex;
+    gap: 20px;
+  }
+  .navbar-logo {
+    height: 100%;
+    box-sizing: border-box;
+    padding: 5px;
+    aspect-ratio: 1;
+  }
+  .navbar-title {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 10px;
+    font-size: 35px;
+    color: var(--vk-text);
+    font-family: var(--vk-title-font);
+  }
+  .navbar-items {
+    flex: 1;
+    justify-content: flex-end; 
+    display: flex;
+    padding-right: 30px;
+    padding-left: 30px;
+    gap: 15px;
+    box-sizing: border-box;
+    min-width: fit-content;
+  }
+  .navbar-item {
+    text-decoration: none;
+    min-width: fit-content;
+    color: var(--vk-text);
+    font-size: 18px;
+    padding: 3px 8px 3px 8px;
+    position: relative;
+    z-index: 2;
+  }
+  .navbar-item:before {
+    transition: background .15s ease-out;
+    content: '';
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    position: absolute;
+    background-color: transparent;
+    filter: opacity(.3);
+    border-radius: 6px;
+    z-index: -1;
+  }
+  .navbar-item:hover:before {
+    background: var(--vk-background-light);
+  }
+
+  @media (max-width: 492px) {
+    .navbar-header {
+      height: 120px;
+    }
+    .navbar {
+      height: fit-content;
+      padding-bottom: 10px;
+      gap: 0px;
+    }
+    .navbar-items {
+      justify-content: center;
+      gap: 0px;
+    }
+  }
+
+  @media (min-width: 493px) and (max-width: 690px) {
+    .navbar-header {
+      height: 130px;
+    }
+    .navbar {
+      height: fit-content;
+      padding-bottom: 10px;
+      gap: 0px;
+    }
+    .navbar-items {
+      justify-content: center;
+    }
+  }
+</style>
